d8acce93729d0d7511cc8592764cbabfa639e309
dudem8z7l <44660089+dudem8z7l@users.noreply.github.com>
2019-01-25 15:55:49 +0700

    perform this after running anova_num_vs_cat.py

---
+++ 1b.pre-analysis/tukey_num_vs_cat.py
@@ -0,0 +1,18 @@
+def tukey_num_vs_cat(df, input_num_col, target_cat_col):
+    '''
+    AIM    -> Find pairwise between numerical variables and categorical variables after inspecting their relationship with ANOVA (in this case, each numerical input will be inspected with categorical target column)
+     
+    INPUT  -> df, list of input numerical variables and target variables (only applied in classification problem)
+    
+    OUTPUT -> pairwise significance of each input numerical variables toward target variable 
+    ------
+    '''
+    from statsmodels.stats.multicomp import pairwise_tukeyhsd
+    for col in input_num_col:
+        tukey = pairwise_tukeyhsd(endog = df[col],    
+                                  groups = df[target_cat_col],
+                                  alpha = 0.05)
+        tukey.plot_simultaneous(xlabel = col)                      
+        print("===" + col +"===")
+        print(tukey.summary())
+        print("\n")
