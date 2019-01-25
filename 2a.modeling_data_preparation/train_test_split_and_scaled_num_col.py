4a730354a061ab13de373a7649f13a4192d2e6f3
dudem8z7l <44660089+dudem8z7l@users.noreply.github.com>
2019-01-25 17:18:25 +0700

    update argument used
    splitting to input numerical and categorical instead of input column only

--- 2a.modeling_data_preparation/train_test_split_and_scaled_num_col.py
+++ 2a.modeling_data_preparation/train_test_split_and_scaled_num_col.py
@@ -1,15 +1,15 @@
-def train_test_split_and_scaled_num_col(df_model, input_col, target_col):
+def train_test_split_and_scaled_num_col(df_model, input_cat_col, input_num_col, target_col):
     from sklearn import preprocessing
     from sklearn.model_selection import train_test_split
-    
+
     #train test split
-    X_train, X_test, y_train, y_test = train_test_split(df[input_col], df[target_col], test_size=0.4, random_state=1)
-    
+    X_train, X_test, y_train, y_test = train_test_split(df[input_cat_col + input_num_col], df[target_col], 
+                                                        test_size = 0.4, random_state = 1)
+
     # min max scaled for numerical columns
     scaler = preprocessing.MinMaxScaler()
     # fit transform on train dataset
-    X_train[X_train.select_dtypes(include=['float', 'integer']).columns] = scaler.fit_transform(X_train[X_train.select_dtypes(include=['float', 'integer']).columns])
+    X_train[input_num_col] = scaler.fit_transform(X_train[input_num_col])
     # only transform on test dataset
-    X_test[X_test.select_dtypes(include=['float', 'integer']).columns] = scaler.transform(X_test[X_test.select_dtypes(include=['float', 'integer']).columns])
-    
+    X_test[input_num_col] = scaler.transform(X_test[input_num_col])
     return scaler, X_train, X_test
