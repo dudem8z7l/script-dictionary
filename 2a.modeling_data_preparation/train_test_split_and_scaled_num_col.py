760da595388b1f210b9c9885de0d87f70fd427d9
dudem8z7l <44660089+dudem8z7l@users.noreply.github.com>
2019-01-28 13:58:43 +0700

    minor bug

--- 2a.modeling_data_preparation/train_test_split_and_scaled_num_col.py
+++ 2a.modeling_data_preparation/train_test_split_and_scaled_num_col.py
@@ -3,7 +3,7 @@ def train_test_split_and_scaled_num_col(df_model, input_cat_col, input_num_col, 
     from sklearn.model_selection import train_test_split
 
     #train test split
-    X_train, X_test, y_train, y_test = train_test_split(df[input_cat_col + input_num_col], df[target_col], 
+    X_train, X_test, y_train, y_test = train_test_split(df_model[input_cat_col + input_num_col], df[target_col], 
                                                         test_size = 0.4, random_state = 1)
 
     # min max scaled for numerical columns
